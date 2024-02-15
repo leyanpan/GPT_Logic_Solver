51d8f0bcb7bcd1198805b23c1bfdc6412949bbcc
warmstrong9 <wsa@gatech.edu>
2024-02-15 02:32:56 +0000

    added option to shift input randomly within context window to train encodings at more positions (don't always left-align)

--- sat_dataset.py
+++ sat_dataset.py
@@ -22,9 +22,10 @@ class CustomTokenizer(PreTrainedTokenizer):
 
 # Custom dataset class
 class SATDataset(Dataset):
-    def __init__(self, file_path, tokenizer, block_size, remove_trace=False):
+    def __init__(self, file_path, tokenizer, block_size, remove_trace=False, shift_within_block=False):
         self.tokenizer = tokenizer
         self.block_size = block_size
+        self.shift_within_block = shift_within_block
         self.examples = []
 
         with open(file_path, 'r') as f:
@@ -37,16 +38,55 @@ class SATDataset(Dataset):
 
     def __len__(self):
         return len(self.examples)
+    
+    def left_pad(self, tensor, pad_size, pad_with, block_size):
+        p = torch.tensor([pad_with] * pad_size)
+
+        return torch.cat((p, tensor))[0:block_size]
 
     def __getitem__(self, i):
         # Tokenize the line
         tokens = self.tokenizer(self.examples[i], truncation=True, padding='max_length', max_length=self.block_size, return_tensors="pt")
+
         # Extract input_ids, attention mask as a tensor
         input_ids = tokens["input_ids"].squeeze()
         attention_mask = tokens["attention_mask"].squeeze()
 
         # Create a label that's the input sequence shifted left by one
         labels = torch.cat((input_ids[1:], torch.tensor([self.tokenizer.pad_token_id])))
+
+        if self.shift_within_block:
+            # first get length of unmasked input and determine how much left
+            # padding is needed
+            input_len = torch.sum(attention_mask)
+            size_left_pad = torch.randint(high=(block_size - input_len), size=(1,)).item()
+
+            input_ids = self.left_pad(input_ids, size_left_pad, self.tokenizer.pad_token_id, block_size)
+            labels = self.left_pad(labels, size_left_pad, self.tokenizer.pad_token_id, block_size)
+            attention_mask = self.left_pad(attention_mask, size_left_pad, 0, block_size)
         
         # Return a dictionary with input_ids and labels
         return {"input_ids": input_ids, "labels": labels, "attention_mask": attention_mask}
+
+
+if __name__ == "__main__":
+    # Test the dataset
+    max_id = 30
+    custom_tokens = [str(i) for i in range(max_id + 1)] + ["-", "[SEP]", "SAT", "UNSAT", "[EOS]", "[UNK]"]
+
+    tokenizer = CustomTokenizer(custom_tokens)
+    tokenizer.add_special_tokens({'pad_token': '[EOS]'})
+
+    block_size = 800
+
+    DATASET_PATH = "./datasets/SAT_6_10/train.txt"
+    dataset = SATDataset(
+        DATASET_PATH, 
+        tokenizer, 
+        block_size=block_size, 
+        remove_trace=False,
+        shift_within_block=True)
+    
+    i = torch.randint(0, len(dataset), (1,)).item()
+    test_item = dataset.__getitem__(i)
+    print(f"Example item: \n{test_item}")

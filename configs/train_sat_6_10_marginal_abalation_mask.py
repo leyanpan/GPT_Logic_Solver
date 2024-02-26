c104a9a27d2d6d01084bed0c8173c37d968d421f
Leyan Pan <leyanlukepan@outlook.com>
2024-02-25 17:22:55 -0800

    Add new ablation experiemnts

---
+++ configs/train_sat_6_10_marginal_abalation_mask.py
@@ -0,0 +1,8 @@
+block_size = 850
+batch_size = 10
+out_dir = 'models/sat-6-10-marginal-abalation-tokenizer'
+dataset = "datasets/SAT_6_10_Marginal_Old"
+old_tokenizer = True
+rand_pos = False
+perm_vars = False
+mask_formula = True
---
+++ configs/train_sat_6_10_marginal_abalation_pos.py
@@ -0,0 +1,8 @@
+block_size = 850
+batch_size = 10
+out_dir = 'models/sat-6-10-marginal-abalation-pos'
+dataset = "datasets/SAT_6_10_Marginal"
+old_tokenizer = True
+rand_pos = True
+perm_vars = False
+mask_formula = False
